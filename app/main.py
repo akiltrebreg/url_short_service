246c618aff0ce4bc62d4b250d33bd0c7fad55a5d
akiltrebreg <akiltrebreg@yandex.ru>
2025-03-20 15:54:20 +0300

    Change redis version and import

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
-import redis.asyncio as aioredis
+import redis.asyncio as redis
 import os
 
 from .routes import router
@@ -13,8 +13,8 @@ REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 
 @app.on_event("startup")
 async def startup():
-    redis = aioredis.from_url(REDIS_URL, encoding="utf8", decode_responses=True)
-    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
+    redis_client = redis.from_url(REDIS_URL, encoding="utf8", decode_responses=True)
+    FastAPICache.init(RedisBackend(redis_client), prefix="fastapi-cache")
 
     # Запуск планировщика задач
     start_scheduler()
